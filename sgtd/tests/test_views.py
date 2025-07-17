e5151f1c3bb92d740799d52c8d3694fe2d46ee15
Luis Leiva <luis.leiva@utp.ac.pa>
2025-07-17 00:28:27 -0500

    fix: update import paths in test_views.py to reflect new module structure

--- sgtd/tests/test_views.py
+++ sgtd/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from unittest.mock import patch
 
-from users.tests.factories import UserFactory, AdminUserFactory
+from app.tests.factories import UserFactory, AdminUserFactory
 from .factories import (
     SpeakersFactory,
     TeamsFactory,
